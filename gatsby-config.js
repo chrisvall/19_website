c13a67aebf56280d240a91298f67506ec9fd7ad7
Christine Vallaure <christinevallaure@gmail.com>
2019-07-18 11:56:27 +0200

    First commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Christine Vallaure`,
+    description: `Berlin based UX/UI designer with a passion for coding`,
     author: `@gatsbyjs`,
   },
   plugins: [
