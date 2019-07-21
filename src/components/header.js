c042549b452e02ea7c0508a3bcabbc78b5646266
Christine Vallaure <christinevallaure@gmail.com>
2019-07-21 13:32:00 +0200

    first testing

--- src/components/header.js
+++ src/components/header.js
@@ -1,24 +1,41 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
+import Link from 'gatsby-link'
+import './header.css'
+
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-  
-  </header>
+
+
+  <div className="Header">
+    <div className="HeaderGroup">
+      {/* <Link to="/"><img width="30" /></Link> */}
+      {/* <Link to="/">Home</Link> */}
+      <Link to="/work">work</Link>
+      <Link to="/articles">articles</Link>
+      <Link to="/about">about</Link>
+      <Link to="/contact">contact</Link>
+    </div>
+  </div>
+
+
 )
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
 
-Header.defaultProps = {
-  siteTitle: ``,
-}
+// window.onscroll = function() {myFunction()};
+
+// var navbar = document.getElementsByClassName("Header");
+// var sticky = navbar.offsetTop;
+
+// function myFunction() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.className.add("sticky")
+//   } 
+// }
+
+
+
+
+
+
 
 export default Header
