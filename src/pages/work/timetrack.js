d0e83b895fec29de7c868b911d5c7d51ca7bd9c7
Christine Vallaure <christinevallaure@gmail.com>
2019-08-26 11:24:27 +0200

    changed back text into arrow

--- src/pages/work/timetrack.js
+++ src/pages/work/timetrack.js
@@ -4,6 +4,8 @@ import { Link } from "gatsby"
 import Layout from "../../components/layout"
 import SEO from "../../components/seo"
 import Img from "gatsby-image"
+import back from "../../images/backarrow.svg"
+
 
 // fix const name
 const PortfolioPage = () => (
@@ -12,14 +14,16 @@ const PortfolioPage = () => (
     <div className="Margin">
         <div className="Hero">
             <div className="HeroGroup Portfolio">
-            <div className="Backarrow"><Link to="/">Back</Link></div>
+            <div className="Backarrow"><Link to="/"> <img src={back} /></Link></div>
                 <h1>TimeTrack – an app <br></br>for lorem ipsum dorlor sit darum.</h1>
                 <p>APP DESIGN | UI/UX | WIREFRAMING | SPECS </p>
             </div>
         </div>
     </div>
 
-  
+    {/* <img src={"../../images/backarrow.png"} /> */}
+    {/* image={require('../images/imgtest1.jpg')} */}
+
     {/* css for now in layout.scss */}
     <div className="Portfolio">
       <img src={require('../../images/portfolio/timetrack/Vallaure-TimeTrack-01@2x.jpg')} />
