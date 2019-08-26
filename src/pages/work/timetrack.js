1459f493b5408c78c233bb01a124d7f194c9528e
Christine Vallaure <christinevallaure@gmail.com>
2019-08-26 12:24:49 +0200

    text element in portfolio

--- src/pages/work/timetrack.js
+++ src/pages/work/timetrack.js
@@ -16,19 +16,29 @@ const PortfolioPage = () => (
         <div className="Hero">
             <div className="HeroGroup">
             <div className="SpacerHack"></div>
-                <h1>TimeTrack – an app <br></br>for lorem ipsum dorlor sit darum.</h1>
+                <h1>TimeTrack – an app for lorem ipsum dorlor sit darum.</h1>
                 <p>APP DESIGN | UI/UX | WIREFRAMING | SPECS </p>
             </div>
         </div>
     </div>
 
-    {/* <img src={"../../images/backarrow.png"} /> */}
-    {/* image={require('../images/imgtest1.jpg')} */}
 
     {/* css for now in layout.scss */}
     <div className="Portfolio">
       <img src={require('../../images/portfolio/timetrack/Vallaure-TimeTrack-01@2x.jpg')} />
+      <p>TimeTrack is an app that facilitates the tracking of individual project progress. 
+        In a first stage the MVP was created with the most simplistic “track and pause” 
+        functions. Further features can be added such as pre-set budgets, monitoring activity 
+        levels and automating invoices based on preset hourly rates and currencies. Carefully 
+        intertwining UX and UI with a minimalistic approach is what created TimeTrack’s pure 
+        look and solid user experience.</p>
       <img src={require('../../images/portfolio/timetrack/Vallaure-TimeTrack-02@2x.jpg')} />
+
+  
+
+    
+     
+
       <img src={require('../../images/portfolio/timetrack/Vallaure-TimeTrack-03@2x.jpg')} />
       <img src={require('../../images/portfolio/timetrack/Vallaure-TimeTrack-04@2x.jpg')} />
       <img src={require('../../images/portfolio/timetrack/Vallaure-TimeTrack-05@2x.jpg')} />
