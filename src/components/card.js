fa9e15b826e4ab55afc22526cce985ab2fbbb278
Christine Vallaure <christinevallaure@gmail.com>
2019-09-17 15:19:49 +0200

    polished cards

--- src/components/card.js
+++ src/components/card.js
@@ -6,25 +6,32 @@ const Card = props => (
     <div className="Card">
         <a href={props.link}>
         <a href={props.linkext} target="_blank" >
-        <img src={props.image} />
-        <h3>{props.title}</h3>
-        <div className="Stroke"></div>
-        <h2>{props.text}</h2>
+            <img src={props.image} />
+            <div className="Card-TextContainer">
+                <div className="Stroke"></div>
+                <div>
+                    <h3>{props.title}</h3>
+                    <h2>{props.text}</h2>
+                </div>
+            </div>
         </a>
         </a>
     </div>
 )
 
-
 // const Card = props => (
 //     <div className="Card">
-//         <Link to={props.link}>
+//         <a href={props.link}>
+//         <a href={props.linkext} target="_blank" >
 //         <img src={props.image} />
 //         <h3>{props.title}</h3>
 //         <div className="Stroke"></div>
 //         <h2>{props.text}</h2>
-//         </Link>
+//         </a>
+//         </a>
 //     </div>
 // )
+
+
 
 export default Card
