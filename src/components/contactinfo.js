069f37aa04408215a2d28077d3feb13a71774695
Christine Vallaure <christinevallaure@gmail.com>
2019-09-24 14:26:04 +0200

    make contact into a module

---
+++ src/components/contactinfo.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './contactinfo.scss'
+
+
+const Contact = props => (
+  <div className="ContactBox">
+  <h1>Chrsitine Vallaure</h1>
+  <a href="mailto:mail@christinevallaure.com?subject=Mail via Website"><h1>mail@christinevallaure.com</h1></a>
+  <a href="tel:+4917610064926"><h1>+49 176 1006 49 26</h1></a>
+  <p>Berlin – Germany</p>
+</div>
+
+
+)
+
+
+export default Contact
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,20 +1,17 @@
 import React from "react"
-import { Link } from "gatsby"
+
 
 import Layout from "../components/layout"
 import Footer from '../components/Footer';
+import Contact from '../components/contactinfo';
 
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    
-    <div className="ContactBox">
-      <h1>Chrsitine Vallaure</h1>
-      <a href="mailto:mail@christinevallaure.com?subject=Mail via Website"><h1>mail@christinevallaure.com</h1></a>
-      <a href="tel:+4917610064926"><h1>+49 176 1006 49 26</h1></a>
-      <p>Berlin – Germany</p>
-    </div>
+ 
+   
+<Contact></Contact>
 
 
   </Layout>
